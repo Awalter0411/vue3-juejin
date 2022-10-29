a9582c9ec2ecbe30b9516d7fe2b29685c61fc0cd
vikoala <vikoala@foxmail.com>
2022-10-29 15:50:03 +0800

    chore: change to monorepo

---
+++ packages/frontend/src/components/CommentPublish/CommentPublish.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="px-5 py-5 pb-10 bg-white rounded relative">
+    <textarea
+      :placeholder="placeholder"
+      class="h-20 text-xs w-full bg-gray-100 rounded border-blue-500 px-2 py-2 focus:outline-blue-500 resize-none"
+    ></textarea>
+    <div class="absolute right-5">
+      <button class="text-sm bg-blue-500 text-white w-16 rounded h-8">{{text}}</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  placeholder: String,
+  text: String
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ packages/frontend/src/views/PinsView/PinsMyClubView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>PinsMyClubView</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
